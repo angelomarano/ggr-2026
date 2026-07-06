dce501d7b72ab0d880c75bda8a8284bd531e5f3a
Angelo Marano <angelomarano3001@gmail.com>
2026-07-06 22:24:36 +0200

    minor changes

--- check_universe.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, ".")
-from data.universe import load_membership, constituents_at
-
-m = load_membership("data/raw/sp500_membership.csv")
-n = len(constituents_at(m, "2010-01-04"))
-print("Tickers al 2010-01-04:", n, "(atteso: 499)")
